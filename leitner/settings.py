3e43e5450b26464814ef407e98037ae64fcbb9b3
ShadiFgh <shadifarokhghate@gmail.com>
2020-10-05 20:07:22 +0330

    added conf.py

--- leitner/settings.py
+++ leitner/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import . import conf
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uxl)j=c$=w!j!i)vs+j@z4d@ntn@wrwgg)04jgbfa)&6r8jscu'
+SECRET_KEY = conf.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
